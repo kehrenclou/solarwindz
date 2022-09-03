f851737a1d9a5b5ba28c14648bcc0c683ef86698
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-09-02 22:08:38 -0700

    Home Responsive part 1

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,16 +1,19 @@
 /* --------------------------------- imports -------------------------------- */
 import React from "react";
+import Circle from "../Circle";
 
 /* ------------------------------ function View ----------------------------- */
 function Home() {
   /* --------------------------------- return --------------------------------- */
   return (
-    <section className="view view__home">
-      {/* <h1>Home</h1> */}
-      <p>Global Usage of Solar and Wind Power</p>
+     <section className="view view__test">
+      <p className="view__test_text"> Solar and Wind Power</p>
+      <p className="view__test_subtext">a global overview...</p>
+      <Circle />
     </section>
   );
 }
 
 /* --------------------------------- exports -------------------------------- */
 export default Home;
+
--- src/components/routes/Test.js
+++ src/components/routes/Test.js
@@ -1,18 +1,17 @@
 /* --------------------------------- imports -------------------------------- */
 import React from "react";
+import Circle from "../Circle";
 
 /* ------------------------------ function View ----------------------------- */
 function Test() {
   /* --------------------------------- return --------------------------------- */
   return (
     <section className="view view__test">
-      
-      <p className="view__test_text"> Usage of Solar and Wind Power</p>
-      <p className="view__test_subtext">a global overview...</p>
-      <div className="view__circle"></div>
-      <div className="view__circle view__circle_a view__circle_animation_blur_a"></div>
-      <div className="view__circle view__circle_b view__circle_animation_blur_b"></div>
-      <div className="view__circle view__circle_c view__circle_animation_blur_c"></div>
+      <div className="view__test_container">
+        <p className="view__test_text"> Solar and Wind Power</p>
+        <p className="view__test_subtext">a global overview...</p>
+        <Circle />
+      </div>
     </section>
   );
 }
