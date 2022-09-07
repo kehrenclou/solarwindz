21fc5ef0d35f10865965a9345b242fddc7e9c5a8
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-09-07 08:53:25 -0700

    add barchar

---
+++ src/components/BarChart.js
@@ -0,0 +1,18 @@
+/* --------------------------------- imports -------------------------------- */
+import React from "react";
+import{Bar} from 'react-chartjs-2'
+
+/* ----------------------------- function dthree ---------------------------- */
+function BarChart() {
+  return (
+    <section>
+        <Bar/>
+      <div>bonus</div>
+      <div> under construction!</div>
+    
+    </section>
+  );
+}
+
+/* --------------------------------- exports -------------------------------- */
+export default BarChart;
